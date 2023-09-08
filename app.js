42142e23d8ccfce6e4b656ecff17028b44da0568
Olakunle Raheem <olaks.raheem@gmail.com>
2023-09-08 19:19:00 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const dayOfWeek = document.querySelector("#dayOfWeek");
+const utcTime = document.querySelector("#utcTime");
+const username = document.querySelector("#username");
+
+const daysOfWeek = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
+]
+
+dayOfWeek.textContent += daysOfWeek[new Date().getDay()];
+utcTime.textContent += new Date().getUTCMilliseconds();
+username.addEventListener("click", () => {
+
+      window.open("https://github.com/mayor-rhymes", "blank");
+})
