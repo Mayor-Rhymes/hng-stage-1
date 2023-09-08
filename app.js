82d221f09ed0d88d63580decd4a0e4ca786f00f8
Olakunle Raheem <olaks.raheem@gmail.com>
2023-09-08 23:26:30 +0100

    Chore: Removed listener from name

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 const dayOfWeek = document.querySelector("#dayOfWeek");
 const utcTime = document.querySelector("#utcTime");
-const username = document.querySelector("#username");
 
 const daysOfWeek = [
-    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",
-]
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 dayOfWeek.textContent = daysOfWeek[new Date().getDay()];
 
-
 setInterval(() => {
-
-    utcTime.textContent = Date.now();
-
-}, 1)
-
-username.addEventListener("click", () => {
-
-      window.open("https://github.com/mayor-rhymes", "blank");
-})
+  utcTime.textContent = Date.now();
+}, 1);
